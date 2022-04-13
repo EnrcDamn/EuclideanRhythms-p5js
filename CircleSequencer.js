a26882424f8cf85569c70e4fecc6b96a65617d5f
EnrcDamn <enrico.damiani10@gmail.com>
2022-04-13 23:28:49 +0200

    Cleared function parameters names

--- CircleSequencer.js
+++ CircleSequencer.js
@@ -1,5 +1,6 @@
 class CircleSequencer {
-    constructor(centreX, centreY, color, radius, initPulses, initSteps, sound) {
+    constructor(centreX, centreY, color, radius,
+                initPulses, initSteps, sound) {
         this.centreX = centreX;
         this.centreY = centreY;
         this.color = color;
@@ -7,17 +8,26 @@ class CircleSequencer {
         this.sound = sound;
 
         this.isSoundPlayed = false;
-        this.currentTick = 0;
-        this.tick = 0;
+        this.currentNote = 0;
+        this.note = 0;
 
         this.pulsesSlider = createSlider(0, 16, initPulses, 1);
         this.stepsSlider = createSlider(0, 16, initSteps, 1);
         this.pulsesSlider.addClass("mySliders");
         this.stepsSlider.addClass("mySliders");
-        this.sliderPosX = this.pulsesSlider.position(this.centreX - this.radius, this.centreY + 2 * this.radius);
-        this.sliderPosY = this.stepsSlider.position(this.centreX - this.radius, this.centreY + 2 * this.radius + 50);
+        this.pulsesSlider.position(
+            this.centreX - this.radius,
+            this.centreY + 2 * this.radius);
+        this.stepsSlider.position(
+            this.centreX - this.radius,
+            this.centreY + 2 * this.radius + 50);
     }
 
+    setRenderPosition() {
+        translate(this.centreX, this.centreY);
+        noFill();
+        rotate(-PI);
+    }
 
     getSequence() {
         let pulsesValue = this.pulsesSlider.value();
@@ -27,36 +37,41 @@ class CircleSequencer {
         return bjorklund(pulsesValue, stepsValue);
     }
 
-    setRenderPosition() {
-        translate(this.centreX, this.centreY);
-        noFill();
-        rotate(-PI);
-    }
-
-
     drawClockFace() {
         for(let step in this.getSequence()) {
             if(this.getSequence()[step] === 0){
                 fill("#aaaaaa"); // grey
                 noStroke();
-                rect(0, this.radius, this.radius / 10, this.radius / 5, this.radius / 13);
+                rect(
+                    0,
+                    this.radius,
+                    this.radius / 10,
+                    this.radius / 5,
+                    this.radius / 13
+                );
             }
             else if (this.getSequence()[step] === 1) {
                 fill(this.color); // color
                 noStroke();
-                rect(0, this.radius, this.radius / 6, this.radius / 3, this.radius / 10);
+                rect(
+                    0,
+                    this.radius,
+                    this.radius / 6,
+                    this.radius / 3,
+                    this.radius / 10
+                );
             }
             let angle = map(1, 0, this.getSequence().length, 0, 2*PI);
             rotate(angle);
         }
     }
 
-    drawHand(tick) {
+    drawHand(note) {
         rotate(PI / 2);
         stroke("#ffffff"); // white
         strokeWeight(3);
         push();
-        rotate(map(tick, 0, this.getSequence().length, 0, 2* PI));
+        rotate(map(note, 0, this.getSequence().length, 0, 2* PI));
         line(0, 0, this.radius + this.radius / 6, 0);
         pop();
 
@@ -65,25 +80,24 @@ class CircleSequencer {
         ellipse(0, 0, 10, 10);
     }
 
-
-    playPattern(tick) {
-        if (tick != this.currentTick){
+    playPattern(note) {
+        if (note != this.currentNote){
             this.isSoundPlayed = false;
-            if (this.getSequence()[tick] == 1 && this.isSoundPlayed == false) {
+            if (this.getSequence()[note] == 1 && this.isSoundPlayed == false) {
                 this.sound.pan(map(this.centreX, 0, windowWidth, -1.0, 1.0))
                 this.sound.play();
                 this.isSoundPlayed == true;
             }
         }
-        this.currentTick = tick;
+        this.currentNote = note;
     }
 
-    updateTick(stepTime) {
-        if (stepTime <= 0) {
-            if (this.tick < this.getSequence().length - 1)
-                this.tick++;
-            else this.tick = 0;
+    updateNote(sixteenthNoteCount) {
+        if (sixteenthNoteCount <= 0) {
+            if (this.note < this.getSequence().length - 1)
+                this.note++;
+            else this.note = 0;
         }
-        return this.tick;
+        return this.note;
     }
 }
