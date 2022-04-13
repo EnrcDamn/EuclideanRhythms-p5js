c0c7510cdb43b30946876b286d32a84f7b96d50a
EnrcDamn <enrico.damiani10@gmail.com>
2022-04-13 15:58:22 +0200

    Add methods and removed render

--- CircleSequencer.js
+++ CircleSequencer.js
@@ -1,91 +1,84 @@
 class CircleSequencer {
-    constructor(centreX, centreY, color, sequence, radius, sound) {
+    constructor(centreX, centreY, color, radius, initPulses, initSteps, sound) {
         this.centreX = centreX;
         this.centreY = centreY;
         this.color = color;
-        this.sequence = sequence;
         this.radius = radius;
         this.sound = sound;
 
         this.isSoundPlayed = false;
-        this.currentTick = true;
-        this.vibration = false;
-        this.offStep;
-        this.onStep;
-        this.tick;
+        this.currentTick = 0;
+        this.tick = 0;
+        this.maxPulses;
+        this.pulsesSlider = createSlider(0, 16, initPulses, 1);
+        this.stepsSlider = createSlider(0, 16, initSteps, 1);
     }
 
-    clockFace() {
+
+    getSequence() {
+        let pulsesValue = this.pulsesSlider.value();
+        let stepsValue = this.stepsSlider.value();
+        if (pulsesValue > stepsValue) pulsesValue = stepsValue;
+        if (stepsValue < pulsesValue) stepsValue = pulsesValue;
+        return bjorklund(pulsesValue, stepsValue);
+    }
+
+    drawClockFace() {
+        translate(this.centreX, this.centreY);
+        noFill();
         rotate(-PI);
-        for(let step in this.sequence) {
-            if(this.sequence[step] === 0){
+        for(let step in this.getSequence()) {
+            if(this.getSequence()[step] === 0){
                 fill(this.color);
                 noStroke();
-                this.offStep = rect(0, this.radius, this.radius / 10, this.radius / 5);
+                rect(0, this.radius, this.radius / 10, this.radius / 5, this.radius / 13);
             }
-            else if (this.sequence[step] === 1) {
+            else if (this.getSequence()[step] === 1) {
                 fill(100);
                 noStroke();
-                this.onStep = rect(0, this.radius, this.radius / 6, this.radius / 3);
-                if (this.vibration) {
-                    this.onStep.rotate(random(-5/360, 5/360));
-                }
+                rect(0, this.radius, this.radius / 6, this.radius / 3, this.radius / 10);
             }
-            let angle = map(1, 0, this.sequence.length, 0, 2*PI);
+            let angle = map(1, 0, this.getSequence().length, 0, 2*PI);
             rotate(angle);
+
+            fill(100);
+            noStroke();
+            ellipse(0, 0, 10, 10);
         }
     }
 
-    smallRect() {
-
+    drawSliders() {
+        this.pulsesSlider.position(this.centreX - this.radius, this.centreY + 2 * this.radius);
+        this.stepsSlider.position(this.centreX - this.radius, this.centreY + 2 * this.radius + 50);
     }
 
-    hand() {
+    drawHand(tick) {
         rotate(PI / 2);
         stroke(this.color, 20, 100);
-        strokeWeight(2.5);
+        strokeWeight(3);
+        push();
+        rotate(map(tick, 0, this.getSequence().length, 0, 2* PI));
         line(0, 0, this.radius + this.radius / 6, 0);
+        pop();
     }
 
     playPattern(tick) {
         if (tick != this.currentTick){
             this.isSoundPlayed = false;
-            if (this.sequence[tick] == 1 &&this.isSoundPlayed == false && !this.vibration) {
+            if (this.getSequence()[tick] == 1 && this.isSoundPlayed == false) {
                 this.sound.play();
                 this.isSoundPlayed == true;
-                this.vibration = true;
             }
-            this.vibration = false;
         }
         this.currentTick = tick;
     }
 
     updateTick(stepTime) {
         if (stepTime <= 0) {
-            if (this.tick < this.sequence.length - 1)
+            if (this.tick < this.getSequence().length - 1)
                 this.tick++;
             else this.tick = 0;
         }
         return this.tick;
-    }
-
-    render() {
-        // Set up circle starting position
-        
-        translate(this.centreX, this.centreY);
-        //rotate(-PI / 2);
-        noFill();
-
-        this.clockFace();
-
-        // Rotation
-        push();
-        rotate(map(this.updateTick(stepTime), 0, this.sequence.length, 0, 2* PI));
-        this.hand();
-        pop();
-
-        fill(100);
-        noStroke();
-        ellipse(0, 0, 10, 10);
     }
 }
